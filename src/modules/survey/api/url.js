b8e059cf444a84c715639e0d72531ecc095167f5
Hardikj324 <jain22hardik@gmail.com>
2026-02-13 19:11:44 +0530

    Update url.js

--- src/modules/survey/api/url.js
+++ src/modules/survey/api/url.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://ludivina-precorruptive-arleen.ngrok-fre/api";
+// export const BASE_URL = "https://ludivina-precorruptive-arleen.ngrok-fre/api";
 // export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "https://audioupload-dcya.onrender.com/api";
