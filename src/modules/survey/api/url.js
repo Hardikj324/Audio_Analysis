1eae35c4c6398320126da16a2c7c44c69ff394d7
Hardikj324 <jain22hardik@gmail.com>
2026-02-13 13:57:31 +0530

    .
    .

--- src/modules/survey/api/url.js
+++ src/modules/survey/api/url.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://ludivina-precorruptive-arleen.ngrok-free.dev/api";
+export const BASE_URL = "https://ludivina-precorruptive-arleen.ngrok-fre/api";
 // export const BASE_URL = "http://localhost:8000/api";
